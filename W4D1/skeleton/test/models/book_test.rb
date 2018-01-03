81e0e5805f7d91e0ffa5498cc1b15a9267979ac5
Joseph Powers <jepowers@gmail.com>
2018-01-03 08:33:58 -0800

    css exercises

---
+++ W4D1/skeleton/test/models/book_test.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: books
+#
+#  id         :integer          not null, primary key
+#  title      :string           not null
+#  author     :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+require 'test_helper'
+
+class BookTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
