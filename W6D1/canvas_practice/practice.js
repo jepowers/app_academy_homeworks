d7e0369c5772f00a7d4613ef70faf22433f5c49f
Joseph Powers <jepowers@gmail.com>
2018-01-14 16:24:42 -0800

    Fix syntax for canvas practice

--- W6D1/canvas_practice/practice.js
+++ W6D1/canvas_practice/practice.js
@@ -6,11 +6,11 @@ document.addEventListener("DOMContentLoaded", function(){
 
   const ctx = canv.getContext("2d");
   ctx.fillStyle = "blue";
-  ctx.fillRect = (0, 0, 500, 500);
+  ctx.fillRect(0, 0, 500, 500);
 
   ctx.beginPath();
-  ctx.arc(50, 50 , 50, 0 , 2*Math.PI, false);
-  ctx.strokeStyle("orange");
+  ctx.arc(150, 150 , 50, 0 , 2*Math.PI, true);
+  ctx.strokeStyle = "orange";
   ctx.lineWidth = 5;
   ctx.stroke();
   ctx.fillStyle = "red";
